cb297843d205cbff7541c637599a279c610b7fba
ReneTC <sepluk1@gmail.com>
2024-11-29 19:55:56 +0100

    dialouage

---
+++ scenes/maps/tutorial.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func get_and_show_dia(path):
+	DialogueManager.show_dialogue_balloon(load("res://scenes/maps/tut_dialogue/"+ path +".dialogue"))
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_and_show_dia("tutorial")
